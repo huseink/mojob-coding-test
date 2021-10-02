c62d57a788725d009203027be13f563072c33aa9
huseink <huseinkantarci@gmail.com>
2021-10-03 02:04:37 +0300

    feat: create job listing functions to get job data from api

--- src/api-requests/api.ts
+++ src/api-requests/api.ts
@@ -1,5 +1,10 @@
-import { AxiosStatic } from 'axios';
-import { IPage, PositionFunction } from '@/models/models';
+import { AxiosStatic } from "axios";
+import {
+  IPage,
+  PositionFunction,
+  JobListing,
+  Pagination,
+} from "@/models/models";
 
 export default class BaseApi {
   public axios!: AxiosStatic;
@@ -15,9 +20,23 @@ export default class BaseApi {
       .get(`${this.baseUrl}job/position-functions/?page_size=100`)
       .then((response) => response.data);
 
-  /**
-   * TIP: Create a function called getJobListings
-   * Use our public api documentation to find out what endpoint to use
-   * https://test-api.mojob.io/public/docs/
-   */
+  // Get Job Listings with pagination
+  public getPagedJobListings = (
+    pagination: Pagination
+  ): Promise<IPage<JobListing>> => {
+    let queryParameters = "?include_open=False";
+    queryParameters += `&page_size=${pagination.page_size}&use_pagination=True`;
+    return this.axios
+      .get(`${this.baseUrl}job/listings/${queryParameters}`)
+      .then((response) => response.data);
+  };
+
+  // Get Job Listings without pagination
+  public getAllJobListings = (): Promise<JobListing[]> => {
+    let queryParameters =
+      "?include_open=False&use_pagination=False";
+    return this.axios
+      .get(`${this.baseUrl}job/listings/${queryParameters}`)
+      .then((response) => response.data);
+  };
 }
