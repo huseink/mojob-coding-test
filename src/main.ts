180e1d9e194839855cb158df8d201e4b173dfc98
huseink <huseinkantarci@gmail.com>
2021-10-02 21:54:31 +0300

    feat: import vue bootstrap

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,16 @@ import './registerServiceWorker';
 import router from './router';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
+import { BootstrapVue } from 'bootstrap-vue'
+
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false;
 Vue.use(VueAxios, axios);
+// Make BootstrapVue available throughout the project
+Vue.use(BootstrapVue)
 
 new Vue({
   router,
