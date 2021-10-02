eebe3b02ececd73ca65a0a03144f03a84e6b11d4
huseink <huseinkantarci@gmail.com>
2021-10-02 21:53:17 +0300

    feat: create NavBar component

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,29 @@
 <template>
   <div id="app">
+    <NavBar />
     <router-view />
-
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
   </div>
 </template>
 
+<script>
+import NavBar from "@/components/NavBar.vue";
+
+export default {
+  components: {
+    NavBar,
+  },
+};
+</script>
+
+
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;600&display=swap');
 #app {
-  font-family: Avenir, Muli, sans-serif !important;
+  font-family: 'Poppins', sans-serif !important;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
-#nav {
-  padding: 30px;
-}
-#nav a {
-  font-weight: bold;
-  color: #4f4f4f;
-}
-#nav a.router-link-exact-active {
-  color: #46bbb3;
-}
+
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <b-navbar variant="faded" type="light">
+      <b-navbar-brand>
+        <router-link to="/">
+          <img src="../assets/mojob_logo.svg" alt="Mojob Logo" width="130" />
+        </router-link>
+      </b-navbar-brand>
+      |
+      <b-navbar-nav>
+        <b-nav-item to="/" active-class="active" exact>Home</b-nav-item>
+        <b-nav-item to="/about" active-class="active">About</b-nav-item>
+      </b-navbar-nav>
+    </b-navbar>
+  </div>
+</template>
+
+<style>
+.active {
+  color: #46bbb3!important;
+  font-weight: 600!important;
+}
+</style>
