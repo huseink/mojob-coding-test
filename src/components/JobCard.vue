9b6897941477696916293a2dd1cf24117388c798
huseink <huseinkantarci@gmail.com>
2021-10-02 21:55:51 +0300

    feat: create boilerplate design of JobCard and JobFilter components

---
+++ src/components/JobCard.vue
@@ -0,0 +1,41 @@
+<template>
+  <b-card text-variant="dark" title="Position Title" class="job-card">
+    <b-card-text class="job-card-text text-muted">
+      <span>Company displayed name</span>
+      <span class="divider">•</span>
+      <span>Position function</span>
+      <span class="divider">•</span>
+      <span>Due date</span>
+    </b-card-text>
+  </b-card>
+</template>
+
+<script>
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class JobCard extends Vue {}
+</script>
+
+<style scoped>
+.job-card {
+    margin-top:1rem;
+    padding: 0.5rem;
+    text-align: left;
+    background-color: #ffffff !important;
+    box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.05), 0px 1px 10px rgba(0, 0, 0, 0.08),
+    0px 2px 4px rgba(0, 0, 0, 0.14);
+    border-radius: 1rem !important;
+}
+.job-card-text {
+    font-weight: 300;
+}
+.divider {
+  color: #757575;
+  margin-left: 5px;
+  margin-right: 5px;
+}
+.rounded-border {
+  border-radius: 220px !important;
+}
+</style>
---
+++ src/components/JobFilter.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="d-flex justify-content-between">
+    <b-dropdown
+      id="dropdown-1"
+      text="Filter By Position"
+      class="m-md-2"
+      variant="outline-dark"
+      lazy="true"
+    >
+      <b-dropdown-item>First Action</b-dropdown-item>
+    </b-dropdown>
+    <b-dropdown
+      id="dropdown-1"
+      text="5 Per Page"
+      class="m-md-2"
+      variant="outline-dark"
+      lazy="true"
+    >
+      <b-dropdown-item>First Action</b-dropdown-item>
+    </b-dropdown>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.btn {
+  border: none !important;
+}
+</style>
