4d59e9e3ffa0400c1f02f201fc296c2152e31a2a
huseink <huseinkantarci@gmail.com>
2021-10-03 02:05:12 +0300

    feat: display job information accordingly

--- src/components/JobCard.vue
+++ src/components/JobCard.vue
@@ -1,37 +1,42 @@
 <template>
-  <b-card text-variant="dark" title="Position Title" class="job-card">
+  <b-card text-variant="dark" :title="jobListing.job.title" class="job-card">
     <b-card-text class="job-card-text text-muted">
-      <span>Company displayed name</span>
+      <span>{{ jobListing.job.unit.display_name }}</span>
       <span class="divider">•</span>
-      <span>Position function</span>
+      <span>{{ jobListing.job.position_function.name_en }}</span>
       <span class="divider">•</span>
-      <span>Due date</span>
+      <span>{{ jobListing.job.due_date | formatDate }}</span>
     </b-card-text>
   </b-card>
 </template>
 
-<script>
-import { Component, Vue } from "vue-property-decorator";
+<script lang="ts">
+import { JobListing } from "@/models/models";
+import { Component, Prop, Vue } from "vue-property-decorator";
 
 @Component
-export default class JobCard extends Vue {}
+export default class JobCard extends Vue {
+  @Prop({ default: () => [], type: Object as () => JobListing })
+  private jobListing!: JobListing;
+}
 </script>
 
 <style scoped>
 .job-card {
-    margin-top:1rem;
-    padding: 0.5rem;
-    text-align: left;
-    background-color: #ffffff !important;
-    box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.05), 0px 1px 10px rgba(0, 0, 0, 0.08),
+  margin-top: 1rem;
+  padding: 0.5rem;
+  text-align: left;
+  background-color: #ffffff !important;
+  box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.05), 0px 1px 10px rgba(0, 0, 0, 0.08),
     0px 2px 4px rgba(0, 0, 0, 0.14);
-    border-radius: 1rem !important;
+  border-radius: 1rem !important;
 }
 .job-card-text {
-    font-weight: 300;
+  font-weight: 300;
 }
 .divider {
   color: #757575;
+  font-weight: 600;
   margin-left: 5px;
   margin-right: 5px;
 }
