328b51e338bfc0ea9a9bc5e614d41ddc1f29d2d8
huseink <huseinkantarci@gmail.com>
2021-10-03 02:06:26 +0300

    feat: add pagination filter

--- src/components/JobFilter.vue
+++ src/components/JobFilter.vue
@@ -11,18 +11,53 @@
     </b-dropdown>
     <b-dropdown
       id="dropdown-1"
-      text="5 Per Page"
+      :text="selectedPaginationFilter.text"
       class="m-md-2"
       variant="outline-dark"
       :lazy="true"
     >
-      <b-dropdown-item>First Action</b-dropdown-item>
+      <b-dropdown-item
+        v-for="pagination in paginationFilters"
+        :key="pagination.pageSize"
+        @click="onPaginationChange(pagination)"
+      >
+        {{ pagination.text }}
+      </b-dropdown-item>
     </b-dropdown>
   </div>
 </template>
 
-<script>
-export default {};
+<script lang='ts'>
+import { Pagination } from "@/models/models";
+import { Component, Emit, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class JobFilter extends Vue {
+  @Prop()
+  private selectedPaginationFilter!: Pagination;
+
+  private paginationFilters: Pagination[] = [
+    {
+      page_size: 5,
+      text: "5 Per Page",
+      use_pagination: true,
+    },
+    {
+      page_size: 25,
+      text: "25 Per Page",
+      use_pagination: true,
+    },
+    {
+      text: "Display All",
+      use_pagination: false,
+    },
+  ];
+
+  @Emit()
+  onPaginationChange(value: Pagination) {
+    return value;
+  }
+}
 </script>
 
 <style scoped>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,47 @@
 <template>
   <div class="home">
-    <job-feed
-      :job-listings="[]"
-      :position-functions="positionFunctionFilters"
-    />
+    <b-container>
+      <job-filter
+        :selectedPaginationFilter="selectedPaginationFilter"
+        @on-pagination-change="handlePaginationChange"
+      />
+      <job-feed
+        v-if="jobListings.length > 0"
+        :job-listings="jobListings"
+        :position-functions="positionFunctionFilters"
+      />
+    </b-container>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import JobFeed from '@/components/JobFeed.vue';
-import BaseApi from '@/api-requests/api';
-import { IPage, PositionFunction } from '@/models/models';
+import { Component, Vue } from "vue-property-decorator";
+import JobFeed from "@/components/JobFeed.vue";
+import JobFilter from "@/components/JobFilter.vue";
+import BaseApi from "@/api-requests/api";
+import {
+  IPage,
+  JobListing,
+  Pagination,
+  PositionFunction,
+} from "@/models/models";
 
 @Component({
   components: {
     JobFeed,
+    JobFilter,
   },
 })
 export default class Home extends Vue {
   private mojobApi: BaseApi | null = null;
   private positionFunctionFilters: PositionFunction[] = [];
+  private jobListings: JobListing[] = [];
+
+  private selectedPaginationFilter: Pagination = {
+    page_size: 5,
+    text: "5 Per Page",
+    use_pagination: true,
+  };
 
   /**
    * Here you can do necessary request to our
@@ -35,23 +56,62 @@ export default class Home extends Vue {
   private async mounted() {
     // Here is an example on how to retrieve job position function filters
     this.mojobApi = new BaseApi(
-      'https://test-api.mojob.io/public/',
+      "https://test-api.mojob.io/public/",
       this.axios
     );
     try {
-      const jobLocationFiltersResponsePage: IPage<PositionFunction> =
-        await this.mojobApi.getPositionFunctions();
-      if (jobLocationFiltersResponsePage.results) {
-        this.positionFunctionFilters = jobLocationFiltersResponsePage.results;
-        console.log(JSON.stringify(this.positionFunctionFilters, null, 2));
-        console.log(this.positionFunctionFilters);
-      } else {
-        console.log('Failed loading position function filters');
-      }
+      this.fetchPositionFunctions(this.mojobApi);
+      this.fetchJobListings(this.mojobApi, this.selectedPaginationFilter);
     } catch (e) {
-      console.log('Failed loading position function filters');
       console.log(e);
+    }
+  }
+
+  private handlePaginationChange(pagination: Pagination) {
+    this.selectedPaginationFilter = pagination;
+    if (this.mojobApi) {
+      this.fetchJobListings(this.mojobApi, this.selectedPaginationFilter);
+    }
+  }
+
+  private async fetchPositionFunctions(api: BaseApi) {
+    const positionFunctionsResponsePage: IPage<PositionFunction> =
+      await api.getPositionFunctions();
+
+    if (positionFunctionsResponsePage.results) {
+      this.positionFunctionFilters = positionFunctionsResponsePage.results;
+    } else {
+      console.log("Failed loading position function filters");
+    }
+  }
+
+  private async fetchJobListings(api: BaseApi, pagination: Pagination) {
+    if (pagination.use_pagination) {
+      const jobListingsResponsePage: IPage<JobListing> =
+        await api.getPagedJobListings(pagination);
+      if (jobListingsResponsePage.results) {
+        this.jobListings = jobListingsResponsePage.results;
+      } else {
+        console.log("Failed loading job listings");
+      }
+    } else {
+      const jobListingsResponse: JobListing[] = await api.getAllJobListings();
+
+      if (jobListingsResponse && jobListingsResponse.length > 0) {
+        this.jobListings = jobListingsResponse;
+      } else {
+        console.log("Failed loading job listings");
+      }
     }
   }
 }
 </script>
+
+<style scoped>
+.home {
+  background-color: #fafdfd;
+}
+.container {
+  padding: 2rem 0;
+}
+</style>
