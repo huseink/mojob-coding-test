405c83ac6053d03427fe64ef14341ba1821bd48e
huseink <huseinkantarci@gmail.com>
2021-10-03 05:07:27 +0300

    chore: prettify file

--- src/App.vue
+++ src/App.vue
@@ -17,13 +17,12 @@ export default {
 
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;600&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;600&display=swap");
 #app {
-  font-family: 'Poppins', sans-serif !important;
+  font-family: "Poppins", sans-serif !important;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
-
 </style>
